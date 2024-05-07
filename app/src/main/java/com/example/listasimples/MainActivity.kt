6bd42fe54f09e8640bea766620c9335fc136c6ec
laviniabredariol <laviniabredariol@gmail.com>
2024-05-07 15:08:08 -0300

    listasimples

--- app/src/main/java/com/example/listasimples/MainActivity.kt
+++ app/src/main/java/com/example/listasimples/MainActivity.kt
@@ -6,8 +6,12 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,22 +19,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val etnovatarefa = findViewById<EditText>(R.id.etnovatarefa)
         val btadd = findViewById<Button>(R.id.btadd)
+        val tvtitulo = findViewById<TextView>(R.id.tvtitulo)
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            tvtitulo.isVisible = false
+            etnovatarefa.isVisible = true
+            etnovatarefa.isEnabled = true
+            btadd.isVisible = true
+        }
         val lvtarefas = findViewById<ListView>(R.id.lvtarefas)
-
-        val listaTarefas: ArrayList<String> = ArrayList()
-
+        val listaTarefas:ArrayList<String> = ArrayList()
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listaTarefas)
-
-
         lvtarefas.adapter = adapter
 
-        btadd.setOnClickListener{
+        btadd.setOnClickListener {
             if (etnovatarefa.text.isNullOrEmpty()) {
                 Toast.makeText(this, "Digite uma tarefa...", Toast.LENGTH_SHORT).show()
             } else {
                 listaTarefas.add(etnovatarefa.text.toString())
                 adapter.notifyDataSetChanged()
                 etnovatarefa.setText("")
+                etnovatarefa.isVisible = false
+                etnovatarefa.isVisible = true
+            }
+        }
 
                 lvtarefas.setOnItemLongClickListener{ _, _, position, _ ->
 
@@ -51,5 +64,3 @@ class MainActivity : AppCompatActivity() {
                     }
 
                 }
-            }
-        }
